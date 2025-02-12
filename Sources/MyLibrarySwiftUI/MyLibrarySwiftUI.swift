96825ef35b836df9fe4d2ad1352d51fdd9973f19
jecastanosm <jecastanosm@concredito.com.mx>
2025-02-11 19:18:05 -0700

    change

--- Sources/MyLibrarySwiftUI/MyLibrarySwiftUI.swift
+++ Sources/MyLibrarySwiftUI/MyLibrarySwiftUI.swift
@@ -15,7 +15,7 @@ public struct SplashCalculadora: View {
                 .onAppear{
                     Timer.scheduledTimer(withTimeInterval: 5, repeats: true){_ in
                         NavigationLink(destination: CalculadoraUI()){
-                            Text("hace algo")
+                            Text("hace algo nuevo")
                         }
                     }
                 }
