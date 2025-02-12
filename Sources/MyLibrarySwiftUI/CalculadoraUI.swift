579f266006dcb107ca23a4c2a15acb2626b5cdb9
jecastanosm <jecastanosm@concredito.com.mx>
2025-02-11 18:44:54 -0700

    primer push

---
+++ Sources/MyLibrarySwiftUI/CalculadoraUI.swift
@@ -0,0 +1,27 @@
+//
+//  CalculadoraUI.swift
+//  MyLibrarySwiftUI
+//
+//  Created by JECASTAÑOSM on 11/02/25.
+//
+import SwiftUI
+
+@available(iOS 13.0.0, *)
+struct CalculadoraUI: View{
+    
+    var body: some View {
+           NavigationView {
+               VStack {
+                   Text("Hello World")
+               }
+           }
+       }
+    
+}
+
+@available(iOS 13.0.0, *)
+struct ContentView_previewCalculadora: PreviewProvider {
+    static var previews: some View {
+        CalculadoraUI()
+    }
+}
--- Sources/MyLibrarySwiftUI/MyLibrarySwiftUI.swift
+++ Sources/MyLibrarySwiftUI/MyLibrarySwiftUI.swift
@@ -1,2 +1,31 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import SwiftUI
+
+@available(iOS 13.0.0, *)
+public struct SplashCalculadora: View {
+    
+    public init() {}
+    
+    public var body: some View {
+        NavigationView {
+            Text("Calculadora SwiftUI").foregroundColor(.blue).bold()
+                .font(Font.system(size: 30))
+                .onAppear{
+                    Timer.scheduledTimer(withTimeInterval: 5, repeats: true){_ in
+                        NavigationLink(destination: CalculadoraUI()){
+                            Text("Do Something")
+                        }
+                    }
+                }
+        }
+    }
+}
+
+@available(iOS 13.0.0, *)
+struct ContentView_preview: PreviewProvider {
+    static var previews: some View {
+        SplashCalculadora()
+    }
+}
